596031f3f27845d6a77e82fea548ca3785dad254
lakithaomal <lhw150030@utdallas.edu>
2022-01-06 14:48:34 -0600

    Fixed Data Folder On Mints Definitions

--- firmware/mintsXU4/mintsDefinitions.py
+++ firmware/mintsXU4/mintsDefinitions.py
@@ -7,6 +7,7 @@ import yaml
 # Change Accordingly  
 mintsDefinitions = yaml.load(open('mintsXU4/mintsDefinitions.yaml'),Loader=yaml.FullLoader)
 dataFolder       = mintsDefinitions['dataFolder']
+dataFolderReference       = mintsDefinitions['dataFolder'] + "reference"
 dataFolderMQTTReference   = mintsDefinitions['dataFolder'] + "/referenceMqtt"  # The path of your MQTT Reference Data 
 dataFolderMQTT            = mintsDefinitions['dataFolder'] + "/rawMqtt"        # The path of your MQTT Raw Data 
 tlsCert                   = "/etc/ssl/certs/ca-certificates.crt"     # The path of your TLS cert
@@ -65,5 +66,5 @@ print("MQTT Credentials File      : {0}".format(mqttCredentialsFile))
 print("MQTT Broker and Port       : {0}, {1}".format(mqttOn,mqttPort))
 print("Sensor Nodes File          : {0}".format(sensorNodesFile))
 
-dataFolder                = "/home/teamlary/mintsData/raw"
-dataFolderReference       = "/home/teamlary/mintsData/reference"
+#dataFolder                = "/home/teamlary/mintsData/raw"
+#dataFolderReference       = "/home/teamlary/mintsData/reference"
